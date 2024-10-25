b2236b5c0e865d2deaa3c5309c20f9a7569adae1
Danilly Condori <danilly7@gmail.com>
2024-10-26 01:21:49 +0200

    Exercise 3

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
 "use strict";
-const button = document.querySelector('button');
+const button = document.querySelector('#nextJokeBtn');
 const jokeContainer = document.querySelector('.card-joke');
 const options = {
     headers: {
         'Accept': 'application/json'
     }
 };
+if (button) {
+    button.addEventListener('click', newJoke);
+}
+const reportAcudits = [];
 function newJoke() {
     fetch("https://icanhazdadjoke.com/", options)
         .then((res) => res.json())
         .then((data) => {
-        console.log(data);
+        console.log("data:", data);
         if (jokeContainer) {
             jokeContainer.innerText = data.joke;
-        }
-        if (button) {
-            button.addEventListener('click', newJoke);
+            const selectedEmoji = document.querySelector('input[name="emoji"]:checked');
+            const score = selectedEmoji ? parseInt(selectedEmoji.value) : 0;
+            const newReport = {
+                joke: data.joke,
+                score: score,
+                date: new Date().toISOString()
+            };
+            reportAcudits.push(newReport);
+            console.log("Report acudit:", reportAcudits);
         }
     })
         .catch((error) => {
         console.error('Error: sorry no more jokes', error);
     });
 }
-newJoke();
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-const button = document.querySelector('button') as HTMLButtonElement | null;
+const button = document.querySelector('#nextJokeBtn') as HTMLButtonElement | null;
 const jokeContainer = document.querySelector('.card-joke') as HTMLElement | null;
 
 const options = {
@@ -7,16 +7,35 @@ const options = {
     }
 }
 
+if (button) {
+    button.addEventListener('click', newJoke);
+}
+
+const reportAcudits: {
+    joke: string;
+    score: number;
+    date: string;
+}[] = [];
+
 function newJoke() {
     fetch("https://icanhazdadjoke.com/", options)
         .then((res) => res.json())
         .then((data) => {
-            console.log(data);
+            console.log("data:", data);
             if (jokeContainer) {
                 jokeContainer.innerText = data.joke;
-            }
-            if (button) {
-                button.addEventListener('click', newJoke);
+
+                const selectedEmoji = document.querySelector('input[name="emoji"]:checked') as HTMLInputElement;
+                const score = selectedEmoji ? parseInt(selectedEmoji.value) : 0;
+               
+                const newReport = {
+                    joke: data.joke,
+                    score: score,
+                    date: new Date().toISOString()
+                };
+
+                reportAcudits.push(newReport);
+                console.log("Report acudit:", reportAcudits);
             }
         })
         .catch((error) => {
@@ -24,5 +43,4 @@ function newJoke() {
         });
 }
 
-newJoke();
 
